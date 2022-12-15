9530163c972ac2dfe10f331c2630efe761e115c0
Nathan Joudet <nathanjoudet@orange.fr>
2022-12-15 14:23:17 +0100

    feat: ajout de la searchbar :sparkles:

--- projet-netfloux/src/App.js
+++ projet-netfloux/src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import Searchbar from "./components/Searchbar";
 
 function App() {
-  return <div className="App">Bienvenue sur App.js</div>;
+  return <div className="App">
+    <Searchbar />
+  </div>;
 }
 
 export default App;
