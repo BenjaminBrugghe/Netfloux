cab21107a6eae0f0c2432996991beaa339bc3b84
BenjaminBrugghe <Brugghe.b@gmail.com>
2022-12-15 10:25:49 +0100

    Ajout README + Structure Router

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Netfloux
+
+## Description
+
+Ce projet fait partit du module "ui/ux - maquettage" de la formation IB.
+Il a été réalisé en groupe de 3 personnes.
+
+## Prérequis
+
+- [Node.js](https://nodejs.org/en/)
+
+## Installation
+
+- Cloner le projet.
+- Installer les dépendances avec 'npm install'.
+- Lancer le serveur avec 'npm start'.
--- projet-netfloux/src/components/Router.jsx
+++ projet-netfloux/src/components/Router.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 
 const Router = () => {
     return (
         <div>
-
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' />
+                    <Route path='/*' />
+                </Routes>
+            </BrowserRouter>
+            <Outlet />
         </div>
     );
 };
