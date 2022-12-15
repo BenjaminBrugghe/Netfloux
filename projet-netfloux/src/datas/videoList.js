8d0a6c8467ae80ec2dc72371981b3183e49540aa
BenjaminBrugghe <Brugghe.b@gmail.com>
2022-12-15 15:16:48 +0100

    Fix: Modif de la liste pour la description

--- projet-netfloux/src/datas/videoList.js
+++ projet-netfloux/src/datas/videoList.js
@@ -3,42 +3,79 @@ import imageLCDP from "./images/lcdp.jpg";
 export const videoList = [
   {
     name: "La casa de papel",
+    description:
+      "Un homme mystérieux, surnommé le Professeur3, planifie le meilleur braquage jamais réalisé. Pour exécuter son plan, il recrute huit des meilleurs malfaiteurs en Espagne qui n'ont rien à perdre.",
     image: imageLCDP,
+    category: "Série",
+    country: "Espagne",
+    releaseDate: "04/05/2020",
+    subCategory: "Action",
+    length: "1h45",
   },
   {
     name: "La casa de papel",
+    description:
+      "Un homme mystérieux, surnommé le Professeur3, planifie le meilleur braquage jamais réalisé. Pour exécuter son plan, il recrute huit des meilleurs malfaiteurs en Espagne qui n'ont rien à perdre.",
     image: imageLCDP,
+    category: "Série",
+    country: "Espagne",
+    releaseDate: "04/05/2020",
+    subCategory: "Action",
+    length: "1h45",
   },
   {
     name: "La casa de papel",
+    description:
+      "Un homme mystérieux, surnommé le Professeur3, planifie le meilleur braquage jamais réalisé. Pour exécuter son plan, il recrute huit des meilleurs malfaiteurs en Espagne qui n'ont rien à perdre.",
     image: imageLCDP,
+    category: "Série",
+    country: "Espagne",
+    releaseDate: "04/05/2020",
+    subCategory: "Action",
+    length: "1h45",
   },
   {
     name: "La casa de papel",
+    description:
+      "Un homme mystérieux, surnommé le Professeur3, planifie le meilleur braquage jamais réalisé. Pour exécuter son plan, il recrute huit des meilleurs malfaiteurs en Espagne qui n'ont rien à perdre.",
     image: imageLCDP,
+    category: "Série",
+    country: "Espagne",
+    releaseDate: "04/05/2020",
+    subCategory: "Action",
+    length: "1h45",
   },
   {
     name: "La casa de papel",
+    description:
+      "Un homme mystérieux, surnommé le Professeur3, planifie le meilleur braquage jamais réalisé. Pour exécuter son plan, il recrute huit des meilleurs malfaiteurs en Espagne qui n'ont rien à perdre.",
     image: imageLCDP,
+    category: "Série",
+    country: "Espagne",
+    releaseDate: "04/05/2020",
+    subCategory: "Action",
+    length: "1h45",
   },
   {
     name: "La casa de papel",
+    description:
+      "Un homme mystérieux, surnommé le Professeur3, planifie le meilleur braquage jamais réalisé. Pour exécuter son plan, il recrute huit des meilleurs malfaiteurs en Espagne qui n'ont rien à perdre.",
     image: imageLCDP,
+    category: "Série",
+    country: "Espagne",
+    releaseDate: "04/05/2020",
+    subCategory: "Action",
+    length: "1h45",
   },
   {
     name: "La casa de papel",
+    description:
+      "Un homme mystérieux, surnommé le Professeur3, planifie le meilleur braquage jamais réalisé. Pour exécuter son plan, il recrute huit des meilleurs malfaiteurs en Espagne qui n'ont rien à perdre.",
     image: imageLCDP,
-  },
-  {
-    name: "La casa de papel",
-    image: imageLCDP,
-  },
-  {
-    name: "La casa de papel",
-    image: imageLCDP,
-  },
-  {
-    name: "La casa de papel",
-    image: imageLCDP,
+    category: "Série",
+    country: "Espagne",
+    releaseDate: "04/05/2020",
+    subCategory: "Action",
+    length: "1h45",
   },
 ];
