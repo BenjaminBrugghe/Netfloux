3d7f42d8569e214e6ccdd3eef55123612a3c4c73
BenjaminBrugghe <Brugghe.b@gmail.com>
2022-12-16 11:24:37 +0100

    Feat: Ajout du chemin pour affichage layout Film

--- projet-netfloux/src/components/FilmsDescription.jsx
+++ projet-netfloux/src/components/FilmsDescription.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import '../styles/filmsDescription.css'
+import FilmPath from './FilmPath';
 import HeaderBtn from "./HeaderBtn"
+import { videoList } from '../datas/videoList';
 
 const FilmsDescription = ({ film }) => {
     return (
-        <div className='filmDescriptionContainer'>
-            <div className='filmImageContainer'>
-                <img className='filmImage' src={film.image} alt="" />
-            </div>
-            <div className='filmInformations'>
-                <h2 className='filmName'>{film.name}</h2>
-                <div className='filmInfos'>{film.category} - {film.country} - {film.releaseDate} - {film.subCategory} - {film.length}</div>
-                <div className='filmDescription'>{film.description}</div>
-                <div className="filmButtons">
-                    <HeaderBtn type='descriptionBtnPlay descriptionBtn' text="Play" />
-                    <HeaderBtn type='descriptionBtnBA descriptionBtn' text="Bande Annonce" />
-                    <HeaderBtn type='descriptionBtnNotes descriptionBtn' text="Notes" />
+        <>
+            <FilmPath film={videoList[0]} />
+            <div className='filmDescriptionContainer'>
+                <div className='filmImageContainer'>
+                    <img className='filmImage' src={film.image} alt="" />
+                </div>
+                <div className='filmInformations'>
+                    <h2 className='filmName'>{film.name}</h2>
+                    <div className='filmInfos'>{film.category} - {film.country} - {film.releaseDate} - {film.subCategory} - {film.length}</div>
+                    <div className='filmDescription'>{film.description}</div>
+                    <div className="filmButtons">
+                        <HeaderBtn type='descriptionBtnPlay descriptionBtn' text="Play" />
+                        <HeaderBtn type='descriptionBtnBA descriptionBtn' text="Bande Annonce" />
+                        <HeaderBtn type='descriptionBtnNotes descriptionBtn' text="Notes" />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
