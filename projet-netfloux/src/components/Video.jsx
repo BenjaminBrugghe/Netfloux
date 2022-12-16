766bb22d7b84ea18cd58518271f7d068c5c623a3
Nathan-Joudet-Dev <nathanjoudet@orange.fr>
2022-12-16 10:28:31 +0100

    feat: ajout de la video :sparkles:

--- projet-netfloux/src/components/Video.jsx
+++ projet-netfloux/src/components/Video.jsx
@@ -3,13 +3,13 @@ import "../styles/Video.css";
 
 const Video = () => {
   return (
-    <div className='MusiqueContainer'>
-    <iframe 
-      src="https://www.youtube.com/embed/A9oB3ZT37MQ"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    ></iframe>
+    <div>
+      <iframe
+        src="https://www.youtube.com/embed/BBJa32lCaaY"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
     </div>
   );
 };
